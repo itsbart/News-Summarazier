3fb453a6c69f0243ca714733abbac416ad8bcaf4
Jeffrey Spaulding <jeffrey.spaulding@cubrc.org>
2014-11-30 21:36:33 -0500

    Implemented topics tree and added feed button; formatted dates MMM DD, YYYY

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -6,7 +6,7 @@
 // jslint options
 /*----------------------------------------------------------*/
 /*jslint browser: true, todo: true, unparam:true */
-/*global console, $*/
+/*global console, $, moment */
 
 (function () {
     'use strict';
@@ -15,100 +15,151 @@
         $topicsPanel = $("#topics-panel"),
         $topicsLoader = $("#topics-loader"),
         $topicsList = $("#topics-list"),
-        $activeTopic = null;
+        $feedsPanel = $("#feeds-panel"),
+        $feedsList = $("#feeds-list"),
+        $feedsHint = $("#feeds-hint"),
+        $activeTopic = null,
+        ALL_TOPICS = "All Topics",
+        LABEL_KEY = "label",
+        curQueryStr = null,
+        curResults = null,
+        $floatingFeed = $("#floating-feed"),
+        $addFeedButton = $("#add-feed-button");
 
     function onResize() {
         var h = $topicsPanel.height();
         h -= $topicsPanel.find(".panel-heading").outerHeight();
         h -= 15;
         $topicsPanel.find(".panel-body").height(h);
+
+        h = $feedsPanel.height();
+        h -= 15;
+        $feedsPanel.find(".panel-body").height(h);
+    }
+
+    function createResultsList(docs) {
+        var i,
+            doc,
+            entry,
+            j;
+
+        if (docs) {
+            for (i = 0; i < docs.length; i = i + 1) {
+                doc = docs[i];
+
+                // Build the list entry
+                entry = '<div><h4><a href="' + doc.url + '" target="_blank">';
+                for (j = 0; j < doc.title.length; j = j + 1) {
+                    entry += doc.title[j] + "&nbsp;";
+                }
+                entry += '</a><br><small>' + doc.url + '</small></h4><span>';
+                if (doc.date) {
+                    entry += '<em>' + moment(doc.date).format("MMM DD, YYYY") + "</em>&nbsp;-&nbsp;";
+                }
+                if (doc.content && doc.content.length > 0) {
+                    entry += doc.content[0];
+                }
+                entry += '</span></div>';
+                $resultsList.append(entry);
+            }
+        } else {
+            $.bootstrapGrowl("Error creating results list", {type: "danger", delay: 15000});
+        }
     }
 
     function onTopicClick() {
+        var label;
+
         if ($activeTopic) {
             $activeTopic.removeClass("active");
         }
         $activeTopic = $(this);
         $activeTopic.addClass("active");
+
+        $resultsList.empty();
+        label = $activeTopic.data(LABEL_KEY);
+        if (label === ALL_TOPICS) {
+            createResultsList(curResults.response.docs);
+        } else if (label) {
+            $resultsLoader.show();
+            $.ajax({
+                url: "query/cluster",
+                data: {
+                    query: curQueryStr,
+                    cluster: label
+                },
+                dataType: "json"
+            }).done(function (docs) {
+                $resultsLoader.hide();
+                createResultsList(docs);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                $.bootstrapGrowl("Error executing query/cluster", {type: "danger", delay: 15000});
+            });
+        }
+    }
+
+    function createTopicsList(numDocs, clusters) {
+        var i,
+            entry,
+            cluster,
+            label;
+
+        if (clusters) {
+            // Process Topics/Clusters
+
+            // Build the cluster entries
+            entry = '<a href="#" class="list-group-item active">' + ALL_TOPICS + ' (';
+            entry += numDocs + ')</a>';
+            $activeTopic = $(entry);
+            $activeTopic.click(onTopicClick);
+            $activeTopic.data("label", ALL_TOPICS);
+            $topicsList.append($activeTopic);
+
+            for (i = 0; i < clusters.length; i = i + 1) {
+                cluster = clusters[i];
+                entry = '<a href="#" class="list-group-item subtopic">';
+                if (cluster.labels && cluster.labels.length > 0) {
+                    label = cluster.labels[0];
+                    entry += label;
+                }
+                if (cluster.docs) {
+                    entry += " (" + cluster.docs.length + ")";
+                }
+                entry += '</a>';
+
+                // topic click handler
+                entry = $(entry);
+                entry.click(onTopicClick);
+
+                // Save label data for topic click handler
+                entry.data(LABEL_KEY, label);
+
+                $topicsList.append(entry);
+            }
+        } else {
+            $.bootstrapGrowl("Error processing clusters", {type: "danger", delay: 15000});
+        }
     }
 
     function processResults(results) {
-        var i,
-            docs,
-            doc,
-            entry,
-            j,
-            clusters,
-            cluster;
-
         if (results.response && results.response.docs) {
-
-            // Process Docs
-            if (results.response.docs) {
-                docs = results.response.docs;
-                for (i = 0; i < docs.length; i = i + 1) {
-                    doc = docs[i];
-
-                    // Build the list entry
-                    entry = '<div><h4><a href="' + doc.url + '" target="_blank">';
-                    for (j = 0; j < doc.title.length; j = j + 1) {
-                        entry += doc.title[j] + "&nbsp;";
-                    }
-                    entry += '</a><br><small>' + doc.url + '</small></h4><span>';
-                    if (doc.date) {
-                        entry += doc.date + "&nbsp;-&nbsp;";
-                    }
-                    if (doc.content && doc.content.length > 0) {
-                        entry += doc.content[0];
-                    }
-                    entry += '</span></div>';
-                    $resultsList.append(entry);
-                }
-            }
-
-            // Process Topics/Clusters
-            if (results.clusters) {
-                clusters = results.clusters;
-                // Build the cluster entries
-                entry = '<a href="#" class="list-group-item active">All Topics (';
-                entry += docs.length + ')</a>';
-                $activeTopic = $(entry);
-                $activeTopic.click(onTopicClick);
-                $topicsList.append($activeTopic);
-
-                for (i = 0; i < clusters.length; i = i + 1) {
-                    cluster = clusters[i];
-                    entry = '<a href="#" class="list-group-item subtopic">';
-                    for (j = 0; j < cluster.labels.length; j = j + 1) {
-                        if (j !== 0) {
-                            entry += ", ";
-                        }
-                        entry += cluster.labels[j];
-                    }
-                    if (cluster.docs) {
-                        entry += " (" + cluster.docs.length + ")";
-                    }
-                    entry += '</a>';
-
-                    // topic click handler
-                    entry = $(entry);
-                    entry.click(onTopicClick);
-                    $topicsList.append(entry);
-                }
-            }
+            createResultsList(results.response.docs);
+            createTopicsList(results.response.docs.length, results.clusters);
         } else {
             $.bootstrapGrowl("Error processing query", {type: "danger", delay: 15000});
         }
     }
 
-    function onSearch(event, queryStr) {
 
+    function onSearch(event, queryStr) {
+        curQueryStr = queryStr;
         $topicsList.hide();
         $topicsList.empty();
         $topicsLoader.fadeIn();
 
         $resultsList.empty();
         $resultsLoader.fadeIn();
+        $addFeedButton.removeAttr("disabled");
 
         $.ajax({
             url: "query",
@@ -120,11 +171,21 @@
             $topicsLoader.hide();
             $topicsList.show();
             $resultsLoader.hide();
+            curResults = results;
             processResults(results);
 
         }).fail(function (jqXHR, textStatus, errorThrown) {
             $.bootstrapGrowl("Error executing query", {type: "danger", delay: 15000});
         });
+    }
+
+    function addFeed() {
+        var entry;
+        if (curQueryStr) {
+            $feedsHint.hide();
+            entry = '<a href="#" class="list-group-item">' + curQueryStr + '</a>';
+            $feedsList.append(entry);
+        }
     }
 
     $(document).ready(function () {
@@ -145,6 +206,23 @@
 
         $searchBox.search();
         $searchBox.on('searched.fu.search', onSearch);
+
+        $addFeedButton.click(function () {
+            var pos = $addFeedButton.position();
+            $floatingFeed.text(curQueryStr);
+            $floatingFeed.css({
+                top: 9,
+                left: 193
+            });
+            $floatingFeed.show();
+            pos.top += 100;
+            pos.left -= 40;
+            $floatingFeed.animate(pos, "slow", function () {
+                $floatingFeed.fadeOut();
+                addFeed();
+                $addFeedButton.attr("disabled", "disabled");
+            });
+        });
     });
 }());
 
